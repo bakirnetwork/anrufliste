9142d5555d47674cd95a3ef4228b82a4529efb42
Niklas Ravnsborg-Gjertsen <niklasravnsborg@gmail.com>
2015-06-14 20:13:05 +0200

    feat: App Logik integrieren

---
+++ src/calls.php
@@ -0,0 +1,194 @@
+<?php
+
+function getCallArray() {
+	initTable(DB_PREFIX . DB_CALLS, SQL_CALLS);
+
+	$query = 'SELECT * FROM ' . DB_PREFIX . DB_CALLS . ' WHERE 1 ORDER BY call_date';
+
+	$calls = queryMySQLData($query);
+
+	$callArray = array();
+
+	if ($calls != NULL) {
+
+		while($row = $calls->fetch_array()) {
+			if ($row['done_date'] == NULL) {
+
+				$callArray[] = array(
+					'id'       => $row['id'],
+					'vorname'  => $row['contact_forname'],
+					'nachname' => $row['contact_lastname'],
+					'telefon'  => $row['contact_phone'],
+					'datum'    => getDateString(strtotime($row['call_date'])),
+					'betreff'  => $row['call_subject'],
+					'personen' => getAssignedUserIDs($row['id'])
+				);
+			}
+		}
+
+	}
+
+	return $callArray;
+}
+
+function getDoneCallArray() {
+	initTable(DB_PREFIX . DB_CALLS, SQL_CALLS);
+
+	$query = 'SELECT * FROM ' . DB_PREFIX . DB_CALLS.' WHERE 1 ORDER BY call_date';
+
+	$calls = queryMySQLData($query);
+
+	$callArray = array();
+
+	if ($calls != NULL) {
+		while($row = $calls->fetch_array()) {
+			if ($row['done_date'] != NULL) {
+
+				$callArray[] = array(
+					'id'       => $row['id'],
+					'vorname'  => $row['contact_forname'],
+					'nachname' => $row['contact_lastname'],
+					'telefon'  => $row['contact_phone'],
+					'datum'    => getDateString(strtotime($row['call_date'])),
+					'betreff'  => $row['call_subject'],
+					'personen' => getAssignedUserIDs($row['id'])
+				);
+			}
+		}
+	}
+
+	return $callArray;
+}
+
+function getDateString($call_time) {
+
+	$etime = time() - $call_time;
+
+	$a = array(
+		365 * 24 * 60 * 60 => array('Jahr', 'Jahren'),
+		 30 * 24 * 60 * 60 => array('Monat', 'Monaten'),
+		  7 * 24 * 60 * 60 => array('Woche', 'Wochen'),
+		      24 * 60 * 60 => array('Tag', 'Tagen'),
+		           60 * 60 => array('Stunde', 'Stunden'),
+		                60 => array('Minute', 'Minuten')
+	);
+
+	foreach ($a as $secs => $str) {
+		$d = $etime / $secs;
+		if ($d >= 1) {
+			$r = round($d);
+			return 'Vor ' . $r . ' ' . ($r > 1 ? $str[1] : $str[0]);
+		}
+	}
+
+	return 'Gerade eben';
+}
+
+function getAssignedUserIDs($call_id) {
+	initTable(DB_PREFIX . DB_ASSIGNMENTS, SQL_ASSIGNMENTS);
+
+	$query = 'SELECT user_id FROM ' . DB_PREFIX . DB_ASSIGNMENTS . ' WHERE call_id=' . $call_id;
+	$assignments = queryMySQLData($query);
+
+	$userArray = array();
+
+	while($call_assignment = $assignments->fetch_array()) {
+		$userArray[] = $call_assignment['user_id'];
+	}
+
+	return $userArray;
+}
+
+function getAssignedUserNames($call_id) {
+	initTable(DB_PREFIX . DB_ASSIGNMENTS, SQL_ASSIGNMENTS);
+
+	$query = 'SELECT user_id FROM ' . DB_PREFIX . DB_ASSIGNMENTS . ' WHERE call_id=' . $call_id;
+	$result = queryMySQLData($query);
+
+	$assignments = array();
+	while($call_assignment = $result->fetch_array()) {
+		$name = queryMySQLData('SELECT name FROM ' . DB_PREFIX . DB_USERS . ' WHERE id=' . $call_assignment['user_id'])->fetch_array();
+		$assignments[] = $name[0];
+	}
+	return $assignments;
+}
+
+function callDone($id) {
+	initTable(DB_PREFIX . DB_CALLS, SQL_CALLS);
+
+	// check wether call is not already done
+	$date = queryMySQLData('SELECT done_date FROM ' . DB_PREFIX . DB_CALLS . ' WHERE id = ' . $id . ';')->fetch_array();
+	if ($date[0] == NULL) {
+		$date = date('Y-m-d H:i:s');
+		$person_id = getLogState();
+		$query = 'UPDATE ' . DB_PREFIX . DB_CALLS . ' SET done_date = \'' . $date . '\', done_person = ' . $person_id . ' WHERE id = ' . $id . ';';
+		queryMySQLData($query);
+	}
+}
+
+function callUndo($id) {
+	initTable(DB_PREFIX . DB_CALLS, SQL_CALLS);
+
+	// check wether call is already done
+	$date = queryMySQLData('SELECT done_date FROM ' . DB_PREFIX . DB_CALLS . ' WHERE id = ' . $id . ';')->fetch_array();
+	if ($date[0] != NULL) {
+		$query = 'UPDATE ' . DB_PREFIX . DB_CALLS . ' SET done_date = NULL, done_person = NULL WHERE id = ' . $id . ';';
+		queryMySQLData($query);
+	}
+}
+
+function newCall($contact_forname, $contact_lastname, $contact_phone, $call_subject, $call_notes, $call_assignments) {
+	initTable(DB_PREFIX . DB_CALLS, SQL_CALLS);
+	initTable(DB_PREFIX . DB_ASSIGNMENTS, SQL_ASSIGNMENTS);
+
+	$create_datetime = date('Y-m-d H:i:s');
+	$call_date       = $create_datetime;
+	$create_person   = getLogState();
+
+	$query =
+		'INSERT INTO ' . DB_PREFIX . DB_CALLS . '(
+		  create_datetime,
+		  create_person,
+		  call_date,
+		  call_subject,
+		  call_notes,
+		  contact_forname,
+		  contact_lastname,
+		  contact_phone
+		) VALUES (
+		  \'' . $create_datetime . '\',
+		  ' . $create_person . ',
+		  \'' . $call_date . '\',
+		  \'' . $call_subject . '\',
+		  \'' . $call_notes . '\',
+		  \'' . $contact_forname . '\',
+		  \'' . $contact_lastname . '\',
+		  \'' . $contact_phone . '\'
+		);';
+
+	queryMySQLData($query);
+
+	$call_id = queryMySQLData('SELECT * FROM ' . DB_PREFIX . DB_CALLS . ' ORDER BY id DESC LIMIT 1')->fetch_array()['id'];
+
+	$query =
+		'INSERT INTO ' . DB_PREFIX . DB_ASSIGNMENTS . ' (
+		  call_id,
+		  user_id
+		) VALUES ';
+
+	$count = count($call_assignments);
+	$i = 0;
+
+	foreach ($call_assignments as $number => $user_id) {
+
+		if ($i >= $count-1) {
+			$query = $query . '(' . $call_id . ',' . $user_id . '); ';
+		} else {
+			$query = $query . '(' . $call_id . ',' . $user_id . '),';
+		}
+
+		$i++;
+	}
+
+	queryMySQLData($query);
+}
--- src/index.php
+++ src/index.php
@@ -1,10 +1,135 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require(__DIR__ . '/vendor/autoload.php');
+require(__DIR__ . '/bakir-anrufliste-config.php');
+require(__DIR__ . '/calls.php');
 
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 
-// variables and arrays to use in the template go here
-$context = array('foo' => 'bar');
-echo $twig->render('home.twig', $context);
+initLogSys();
+
+$page = 'login';
+$context = array();
+
+if (isset($_GET['logout'])) {
+	logUserOut();
+	header('Location: index.php');
+}
+
+if (isset($_GET['login'])) {
+	if (isset($_POST['username']) && isset($_POST['password'])) {
+		if (isset($_POST['keeplog'])) {
+			logUserIn($_POST['username'], $_POST['password'], true);
+		} else {
+			logUserIn($_POST['username'], $_POST['password'], false);
+		}
+	}
+	header('Location: index.php');
+}
+
+if (isset($_GET['newcall'])) {
+	if (
+		isset($_POST['forename']) &&
+		isset($_POST['lastname']) &&
+		isset($_POST['phone']) &&
+		isset($_POST['subject'])
+	) {
+		newCall($_POST['forename'], $_POST['lastname'], $_POST['phone'], $_POST['subject'], $_POST['notes'], $_POST['assignments']);
+	}
+	header('Location: index.php');
+}
+
+if (isset($_GET['done'])) {
+	if (isset($_POST['done_id'])) {
+		callDone($_POST['done_id']);
+	}
+	header('Location: index.php');
+}
+
+if (isset($_GET['undo'])) {
+	if (isset($_POST['undo_id'])) {
+		callUndo($_POST['undo_id']);
+	}
+	header('Location: index.php');
+}
+
+
+if (getLogState()) {
+
+	if (isset($_GET['account'])) {
+
+		if (isset($_POST['password_old']) && isset($_POST['password_new']) && isset($_POST['password_repeat'])) {
+
+			if ($_POST['password_new'] == $_POST['password_repeat']) {
+				$password_result = !resetPassword($_POST['password_old'], $_POST['password_new']);
+			} else {
+				$password_result = true;
+			}
+
+			if ($password_result == false) {
+				header('Location: index.php');
+			}
+		}
+
+		$page = 'account';
+		$context = array(
+			'form_path'            => 'index.php?account=true',
+			'form_password_old'    => 'password_old',
+			'form_password_new'    => 'password_new',
+			'form_password_repeat' => 'password_repeat',
+			'password_error'       => isset($password_result) ? $password_result : false
+		);
+
+	} else {
+
+		// set assignment ids
+		$query = 'SELECT * FROM ' . DB_PREFIX . DB_USERS . ' WHERE 1';
+		$user = queryMySQLData($query);
+		$assignlist = array();
+
+		while($row = $user->fetch_array()) {
+			$assignlist[] = array(
+				'id'   => $row['id'],
+				'name' => $row['name']
+			);
+		}
+
+		$context = array(
+			'user_name'       => getSingleUserData('name'),
+			'user_id'         => getLogState(),
+			'calls_undone'    => getCallArray(),
+			'calls_done'      => getDoneCallArray(),
+			'users'           => $assignlist,
+			'account_path'    => 'index.php?account=true',
+			'done_path'       => 'index.php?done=true',
+			'undo_path'       => 'index.php?undo=true',
+			'form_path'       => 'index.php?newcall=true',
+			'form_forename'   => 'forename',
+			'form_lastname'   => 'lastname',
+			'form_phone'      => 'phone',
+			'form_subject'    => 'subject',
+			'form_assignment' => 'assignments',
+			'form_notes'      => 'notes'
+		);
+
+		$page = 'home';
+	}
+} else {
+	$context = array(
+		'form_path'          => 'index.php?login=true',
+		'form_user_name'     => 'username',
+		'form_password_name' => 'password',
+		'form_keeplog_name'  => 'keeplog',
+		'login_error'        => false
+	);
+	$page = 'login';
+}
+
+if ($page == 'home') {
+	echo $twig->render('home.twig', $context);
+} else if ($page == 'login') {
+	echo $twig->render('login.twig', $context);
+} else if ($page == 'account') {
+	echo $twig->render('account.twig', $context);
+}
