a93d8d64f98679062152b127f0243820919b2d64
Niklas Ravnsborg-Gjertsen <niklasravnsborg@gmail.com>
2015-06-14 13:36:49 +0200

    refactor: nur php-components-config.php als Beispiel hinzufügen

---
+++ src/php-components-config-sample.php
@@ -0,0 +1,19 @@
+<?php
+
+// php-components-configuration file
+
+// MySql-settings
+define('DB_NAME',     'your_db_name');
+define('DB_USER',     'your_db_user');
+define('DB_PASSWORD', 'your_db_password');
+define('DB_HOST',     'your_db_host');
+define('DB_CHARSET',  'utf8');
+
+// MySQL database table prefix.
+define('DB_PREFIX', 'bc_');
+
+// MySQL user table name (without prefix)
+define('DB_USERS', 'users');
+
+// load php-components files
+require(ABSPATH . 'php-components.php');
