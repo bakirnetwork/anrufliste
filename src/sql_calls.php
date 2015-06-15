4f45a3e16d109e1264fea7a683cff61305b06239
Richard Keil <richartkeil@gmail.com>
2015-06-15 20:28:14 +0200

    fix: Name wird nach 11 Zeichen abgeschnitten - varchar Maximum verlängern

--- src/sql_calls.php
+++ src/sql_calls.php
@@ -9,8 +9,8 @@ define(
 	  `call_date` datetime NOT NULL,
 	  `call_subject` varchar(100) NOT NULL,
 	  `call_notes` longtext,
-	  `contact_forname` varchar(11) NOT NULL,
-	  `contact_lastname` varchar(11) NOT NULL,
+	  `contact_forname` varchar(100) NOT NULL,
+	  `contact_lastname` varchar(100) NOT NULL,
 	  `contact_phone` varchar(100) NOT NULL,
 	  `done_date` datetime DEFAULT NULL,
 	  `done_person` int(11) DEFAULT NULL
