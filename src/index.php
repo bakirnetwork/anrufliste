cf59a1582ff87391a95a04ea18be7276eb20240b
Richard Keil <richartkeil@gmail.com>
2015-06-16 20:31:24 +0200

    refactor: Weiterleitungssystem neu aufsetzen

--- src/index.php
+++ src/index.php
@@ -6,175 +6,261 @@ require(__DIR__ . '/vendor/autoload.php');
 require(__DIR__ . '/bakir-anrufliste-config.php');
 require(__DIR__ . '/calls.php');
 
+
+// init twig
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 $slack = new Maknz\Slack\Client(SLACK_WEBHOOK, ['link_names' => true]);
-
-initLogSys();
-
-$page = 'login';
 $context = array();
 
-if (isset($_GET['logout'])) {
-	logUserOut();
-	header('Location: /');
-}
+// init php-components
+initLogSys();
 
-if (isset($_GET['login'])) {
-	if (isset($_POST['username']) && isset($_POST['password'])) {
-		if (isset($_POST['keeplog'])) {
-			logUserIn($_POST['username'], $_POST['password'], true);
-		} else {
-			logUserIn($_POST['username'], $_POST['password'], false);
-		}
-	}
-	header('Location: /');
-}
-
-if (isset($_GET['newcall'])) {
-	if (
-		isset($_POST['forename']) &&
-		isset($_POST['lastname']) &&
-		isset($_POST['phone']) &&
-		isset($_POST['subject'])
-	) {
-		newCall($_POST['forename'], $_POST['lastname'], $_POST['phone'], $_POST['subject'], $_POST['notes'], $_POST['assignments']);
-
-		function assignedNames() {
-			$id_query = '';
-
-			foreach($_POST['assignments'] as $assignment) {
-				$id_query = $id_query . $assignment . ',';
-			}
-
-			$id_query = trim($id_query, ',');
-
-			$query = 'SELECT * FROM ' . DB_PREFIX . DB_USERS . ' WHERE id in (' . $id_query . ')';
-			$user = queryMySQLData($query);
-			$namelist = [];
-
-			while($row = $user->fetch_array()) {
-				$namelist[] = $row['name'];
-			}
-
-			return $namelist;
-		}
-
-		// generate and send Slack message
-		$namelist = assignedNames();
-		$slackPoke = '';
-		$anrede = 'Dir';
-
-		if (count($namelist) > 1) {
-			$anrede = 'Euch';
-		}
-
-		foreach($namelist as $i=>$name) {
-			$slackPoke .= '@' . $name;
-
-			if ($i <= count($namelist) -3) {
-				$slackPoke .= ', ';
-			} elseif ($i == count($namelist) -2) {
-				$slackPoke .= ' und ';
-			}
-		}
-
-		$message  = 'Hallo ' . $slackPoke . '! ';
-		$message .= $anrede . ' wurde ein *neuer Anruf* ';
-		$message .= 'von @' . getSingleUserData('name') . ' zugewiesen. ';
-		$message .= '<http://' . $_SERVER['SERVER_NAME'] . '|Hier ansehen> für Details.';
-
-		$slack->send($message);
-
-	}
-
-	header('Location: /');
-}
-
-if (isset($_GET['done'])) {
-	if (isset($_POST['done_id'])) {
-		callDone($_POST['done_id']);
-	}
-	header('Location: /');
-}
-
-if (isset($_GET['undo'])) {
-	if (isset($_POST['undo_id'])) {
-		callUndo($_POST['undo_id']);
-	}
-	header('Location: /');
-}
-
+// init general variables
+$page = 'login';
+// meaning of error variables
+// 0 - everything is ok
+// 1 - internal error
+// 2 - empty field
+// 3 - unallowed field entry
+// 4 - something different, specified behind the initialisation
+$login_error = 0;
+$password_reset_error = 0; // 4 - password_reset_new and password_reset_repeat doeas not match
+$call_new_error = 0;
+$call_done_error = 0;
+$call_undo_error = 0;
 
 if (getLogState()) {
 
-	if (isset($_GET['account'])) {
-
-		if (isset($_POST['password_old']) && isset($_POST['password_new']) && isset($_POST['password_repeat'])) {
-
-			if ($_POST['password_new'] == $_POST['password_repeat']) {
-				$password_result = !resetPassword($_POST['password_old'], $_POST['password_new']);
-			} else {
-				$password_result = true;
-			}
-
-			if ($password_result == false) {
-				header('Location: /');
-			}
-		}
-
-		$page = 'account';
-		$context = array(
-			'form_path'            => '?account=true',
-			'form_password_old'    => 'password_old',
-			'form_password_new'    => 'password_new',
-			'form_password_repeat' => 'password_repeat',
-			'password_error'       => isset($password_result) ? $password_result : false
-		);
-
-	} else {
-
-		// set assignment ids
-		$query = 'SELECT * FROM ' . DB_PREFIX . DB_USERS . ' WHERE 1';
-		$user = queryMySQLData($query);
-		$assignlist = array();
-
-		while($row = $user->fetch_array()) {
-			$assignlist[] = array(
-				'id'   => $row['id'],
-				'name' => $row['name']
-			);
-		}
-
-		$context = array(
-			'user_name'       => getSingleUserData('name'),
-			'user_id'         => getLogState(),
-			'calls_undone'    => getCallArray(),
-			'calls_done'      => getDoneCallArray(),
-			'users'           => $assignlist,
-			'account_path'    => '?account=true',
-			'done_path'       => '?done=true',
-			'undo_path'       => '?undo=true',
-			'form_path'       => '?newcall=true',
-			'form_forename'   => 'forename',
-			'form_lastname'   => 'lastname',
-			'form_phone'      => 'phone',
-			'form_subject'    => 'subject',
-			'form_assignment' => 'assignments',
-			'form_notes'      => 'notes'
-		);
-
-		$page = 'home';
+	// log out
+	if (isset($_POST['logout'])) {
+		logUserOut();
+		header('Location: index.php?site=login');
 	}
+	// change password
+	if (isset($_POST['password_reset'])) {
+		if (
+			isset($_POST['password_reset_old']) &&
+			isset($_POST['password_reset_new']) &&
+			isset($_POST['password_reset_repeat'])
+		) {
+			if ($_POST['password_reset_new'] == $_POST['password_reset_repeat']) {
+				$password_reset_result = resetPassword($_POST['password_reset_old'], $_POST['password_reset_new']);
+				if ($password_reset_result) {
+					header('Location: index.php?site=home');
+				} else {
+					$password_reset_error = 1;
+				}
+			} else {
+				$password_reset_error = 4;
+			}
+		} else { $password_reset_error = 2; }
+	}
+
+	// new call
+	if (isset($_POST['call_new'])) {
+		if (
+			isset($_POST['call_new_forename']) &&
+			isset($_POST['call_new_lastname']) &&
+			isset($_POST['call_new_phone']) &&
+			isset($_POST['call_new_subject'])
+		) {
+			$call_new_result = newCall($_POST['call_new_forename'], $_POST['call_new_lastname'], $_POST['call_new_phone'], $_POST['call_new_subject'], $_POST['call_new_notes'], $_POST['call_new_assignments']);
+
+			if ($call_new_result) {
+
+				function assignedNames() {
+					$id_query = '';
+
+					foreach($_POST['call_new_assignments'] as $assignment) {
+						$id_query = $id_query . $assignment . ',';
+					}
+
+					$id_query = trim($id_query, ',');
+
+					$query = 'SELECT * FROM ' . DB_PREFIX . DB_USERS . ' WHERE id in (' . $id_query . ')';
+					$user = queryMySQLData($query);
+					$namelist = [];
+
+					while($row = $user->fetch_array()) {
+						$namelist[] = $row['name'];
+					}
+
+					return $namelist;
+				}
+
+				// generate and send Slack message
+				$namelist = assignedNames();
+				$slackPoke = '';
+				$anrede = 'Dir';
+
+				if (count($namelist) > 1) {
+					$anrede = 'Euch';
+				}
+
+				foreach($namelist as $i=>$name) {
+					$slackPoke .= '@' . $name;
+
+					if ($i <= count($namelist) -3) {
+						$slackPoke .= ', ';
+					} elseif ($i == count($namelist) -2) {
+						$slackPoke .= ' und ';
+					}
+				}
+
+				$message  = 'Hallo ' . $slackPoke . '! ';
+				$message .= $anrede . ' wurde ein *neuer Anruf* ';
+				$message .= 'von @' . getSingleUserData('name') . ' zugewiesen. ';
+				$message .= '<http://' . $_SERVER['SERVER_NAME'] . '|Hier ansehen> für Details.';
+
+				$slack->send($message);
+
+				header('Location: index.php?site=home');
+			} else {
+				$call_new_error = 1;
+			}
+		} else {
+			$call_new_error = 2;
+		}
+
+	}
+
+	// call done
+	if (isset($_POST['call_done'])) {
+		if (isset($_POST['call_done_id'])) {
+			$call_done_result = callDone($_POST['call_done_id']);
+			if ($call_done_result) {
+				header('Location: index.php?site=home');
+			} else {
+				$call_done_error = 1;
+			}
+		}
+	}
+
+	// undo call
+	if (isset($_POST['call_undo'])) {
+		if (isset($_POST['call_undo_id'])) {
+			$call_undo_result = callUndo($_POST['call_undo_id']);
+			if ($call_undo_result) {
+				header('Location: index.php?site=home');
+			} else {
+				$call_done_error = 1;
+			}
+		}
+	}
+
 } else {
-	$context = array(
-		'form_path'          => '?login=true',
-		'form_user_name'     => 'username',
-		'form_password_name' => 'password',
-		'form_keeplog_name'  => 'keeplog',
-		'login_error'        => false
-	);
-	$page = 'login';
+	// log in
+	if (isset($_POST['login'])) {
+		if (isset($_POST['login_name']) && isset($_POST['login_password'])) {
+			if (isset($_POST['login_keeplog'])) {
+				$login_result = logUserIn($_POST['login_name'], $_POST['login_password'], true);
+			} else {
+				$login_result = logUserIn($_POST['login_name'], $_POST['login_password'], false);
+			}
+
+			if ($login_result) {
+				header('Location: index.php?site=home');
+			}
+			else {
+				$login_error = 1;
+			}
+
+		} else { $login_error = 2; }
+	}
+
+}
+
+// site management
+if (isset($_GET['site'])) {
+
+	// login?
+	if ($_GET['site'] == 'login') {
+
+		// logged in?
+		if (getLogState()) {
+			header('Location: index.php?site=home');
+
+		// not logged in?
+		} else {
+
+			$context = array(
+				'f_home_path'      => 'index.php?site=home',
+				'f_login_submit'   => 'login',
+				'f_login_user'     => 'login_name',
+				'f_login_password' => 'login_password',
+				'f_login_keeplog'  => 'login_keeplog',
+				'login_error'      => $login_error
+			);
+			$page = 'login';
+		}
+
+	// account?
+	} elseif ($_GET['site'] == 'account') {
+		if (getLogState()) {
+			$context = array(
+				'f_account_path'          => 'index.php?site=account',
+				'f_home_path'             => 'index.php?site=home',
+				'f_password_reset_old'    => 'password_reset_old',
+				'f_password_reset_new'    => 'password_reset_new',
+				'f_password_reset_repeat' => 'password_reset_repeat',
+				'f_password_reset_submit' => 'password_reset',
+				'password_reset_error'    => $password_reset_error
+			);
+			$page = 'account';
+		} else {
+			header('Location: index.php?site=login');
+		}
+
+	// home?
+	} elseif ($_GET['site'] == 'home') {
+		if (getLogState()) {
+
+			$query = 'SELECT * FROM ' . DB_PREFIX . DB_USERS . ' WHERE 1';
+			$userList = queryMySQLData($query);
+			$users = array();
+
+			while($row = $userList->fetch_array()) {
+				$users[] = array(
+					'id'   => $row['id'],
+					'name' => $row['name']
+				);
+			}
+
+			$context = array(
+				'user_name'               => getSingleUserData('name'),
+				'user_id'                 => getLogState(),
+				'calls_undone'            => getCallArray(),
+				'calls_done'              => getDoneCallArray(),
+				'users'                   => $users,
+				'f_home_path'             => 'index.php?site=home',
+				'f_account_path'          => 'index.php?site=account',
+				'f_logout_submit'         => 'logout',
+				'f_call_done_submit'      => 'call_done',
+				'f_call_done_id'          => 'call_done_id',
+				'f_call_undo_submit'      => 'call_undo',
+				'f_call_undo_id'          => 'call_undo_id',
+				'f_call_new_forename'     => 'call_new_forename',
+				'f_call_new_lastname'     => 'call_new_lastname',
+				'f_call_new_phone'        => 'call_new_phone',
+				'f_call_new_subject'      => 'call_new_subject',
+				'f_call_new_assignments'  => 'call_new_assignments',
+				'f_call_new_notes'        => 'call_new_notes',
+				'f_call_new_submit'       => 'call_new',
+				'call_new_error'          => $call_new_error,
+				'call_done_error'         => $call_done_error,
+				'call_undo_error'         => $call_undo_error
+			);
+			$page = 'home';
+		} else {
+			header('Location: index.php?site=login');
+		}
+	} else {
+		header('Location: index.php?site=login');
+	}
+
+} else {
+	header('Location: index.php?site=home');
 }
 
 if ($page == 'home') {
